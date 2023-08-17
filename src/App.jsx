faef3b4a9d52a7a5a065d5dabdb7e7af5f0bfaaa
Lautaro Gomez <lauuticavs@gmail.com>
2023-08-17 20:17:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { About } from "./components/About"
+import { Footer } from "./components/Footer"
+import { Navbar } from "./components/Navbar"
+import { Projects } from "./components/Projects"
+import { Skills } from "./components/Skills"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
