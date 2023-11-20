89a17f2b371a104076122ed989171eba87414119
Lautaro Gomez <lautaromgomez95@gmail.com>
2023-11-20 20:11:32 -0300

    se agrego un Json local con data, se esta trabajando en la section projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { dataProyects } from "./json/data.json";
 
 export const Projects = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="container-cards-proyects" id="proyects">
+      {dataProyects.map((proyect) => {
+        return (
+          <div className="container-proyect">
+            <img src={proyect.img} alt="" />
+            <h2>{proyect.title}</h2>
+            <p>{proyect.description}</p>
+            <a href={proyect.deploy} className="">
+              Page
+            </a>
+            <a href={proyect.repo} className="">
+              Code
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
